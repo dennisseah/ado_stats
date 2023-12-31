aea5d910070590fe9ac720296c0eaf8b334f24d3
Dennis Seah <dennis.seah@microsoft.com>
2023-12-31 15:35:19 -0800

    added pydocs

--- configurations/azdo_settings.py
+++ configurations/azdo_settings.py
@@ -4,6 +4,8 @@ from pydantic_settings import BaseSettings
 
 
 class Azdo_Settings(BaseSettings):
+    """Azure DevOps settings"""
+
     azdo_org_name: str
     azdo_project_name: str
     azdo_pat: str
@@ -16,11 +18,22 @@ class Azdo_Settings(BaseSettings):
         extra = "ignore"
 
     def get_area_paths(self) -> list[str]:
+        """Area paths are comma separated values. This method returns a list of area
+           paths.
+
+        Area paths are how Azure DevOps organizes work items.
+        """
         return (
             [r.strip() for r in self.area_paths.split(",")] if self.area_paths else []
         )
 
     def get_name_discard_str(self) -> list[str]:
+        """Name discard strings are comma separated values. This method returns a list
+        of name discard strings.
+
+        Names in the Azure DevOps work items are maybe suffixed with a label e.g.
+        (Contractor)
+        """
         return (
             [r.strip() for r in self.name_discard_str.split(",")]
             if self.name_discard_str
@@ -28,6 +41,9 @@ class Azdo_Settings(BaseSettings):
         )
 
     def get_ignored_repos(self) -> list[str]:
+        """Ignored repos are comma separated values. This method returns a list of
+        git repository names to ignore
+        """
         return (
             [r.strip() for r in self.repos_ignore.split(",")]
             if self.repos_ignore
